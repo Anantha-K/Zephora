183e8b94ddfb950b47d7e9de0317d050d84f403c
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-08-25 09:59:12 +0530

    animation

--- scripts.js
+++ scripts.js
@@ -1,29 +1,29 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// gsap.fromTo("#section1",{
-//         opacity: 1,
-//         scale: 1,
-//     },
-//     {
-//         opacity: 0,
-//         // scale: 1.8,
-//         scrollTrigger: {
-//         trigger: "#section1",
-//         start: "10% 0%",
-//         end: "100% 10%",
-//         scrub: true,
-//         markers:true,
-//         toggleActions: "restart none none none",
-//         },
-//     }
-// )
+gsap.fromTo("#section1",{
+        opacity: 1,
+        scale: 1,
+    },
+    {
+        opacity: 0,
+        // scale: 1.8,
+        scrollTrigger: {
+        trigger: "#section1",
+        start: "10% 0%",
+        end: "200% 10%",
+        scrub: true,
+        // markers:true,
+        toggleActions: "restart none none none",
+        },
+    }
+)
 
 gsap.fromTo("#name",{
     opacity: 1,
     scale: 1,
 },
 {
-    opacity: 0,
+    // opacity: 0,
     scale: 1.8,
     scrollTrigger: {
     trigger: "#section1",
@@ -40,7 +40,7 @@ gsap.fromTo("#logo",{
     scale: 1,
 },
 {
-    opacity: 0,
+    // opacity: 0,
     scale: 0.8,
     scrollTrigger: {
     trigger: "#section1",
@@ -57,7 +57,7 @@ gsap.fromTo("#tgline",{
     scale: 1,
 },
 {
-    opacity: 0,
+    // opacity: 0,
     scale: 0.8,
     scrollTrigger: {
     trigger: "#section1",
